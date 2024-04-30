57ba4172fca8c720ae2fec0f115d756b70456d22
Liam-W-Smith <90705654+Liam-W-Smith@users.noreply.github.com>
2024-04-30 11:12:29 -0400

    R^2

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -14,3 +14,14 @@ class RidgeRegression:
 
     def mse(self, X, y):
         return ((X@self.w - y)**2).mean()
+    
+    def pred(self, X):
+        return (X*self.w).sum(axis = 1)
+    
+    def score(self, X, y):
+        # https://www.ncl.ac.uk/webtemplate/ask-assets/external/maths-resources/statistics/regression-and-correlation/coefficient-of-determination-r-squared.html
+        # R^2 value
+        y_hat = self.pred(X)
+        y_bar = torch.mean(y)
+        return 1 - torch.sum((y - y_hat)**2)/torch.sum((y - y_bar)**2)
+
