41090993f05b316b2d64c1299742d401ab601509
Liam-W-Smith <90705654+Liam-W-Smith@users.noreply.github.com>
2024-05-12 14:34:41 -0400

    linear regression

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -1,5 +1,73 @@
-# Import packages
+# import packages
 import torch
+
+# define linear regression class
+class LinearRegress:
+
+    def __init__(self, penalty = None, lam = 0):
+        self.w = None 
+        self.lam = lam
+        self.penalty = penalty
+
+    def pred(self, X):
+        return X@self.w
+    
+    def mse(self, X, y):
+        return ((self.pred(X) - y)**2).mean()
+    
+    def loss(self, X, y):
+        norm = 0
+        if self.penalty == "l1":
+            norm = torch.sum(torch.abs(self.w[:-1]))
+        if self.penalty == "l2":
+            norm = torch.sum(self.w[:-1]**2)
+        return self.mse(X, y) + self.lam * norm
+    
+    def grad(self, X, y):
+        n = X.size()[1]
+        norm_grad = 0
+        if self.penalty == "l1":
+            norm_grad = torch.sign(self.w)
+            norm_grad[-1] = 0
+        if self.penalty == "l2":
+            norm_grad = 2*self.w
+            norm_grad[-1] = 0
+        return (2/n)*(X.transpose(0, 1))@(X@self.w-y) + self.lam*norm_grad
+    
+    def r2(self, X, y):
+        # https://www.ncl.ac.uk/webtemplate/ask-assets/external/maths-resources/statistics/regression-and-correlation/coefficient-of-determination-r-squared.html
+        # R^2 value
+        y_hat = self.pred(X)
+        y_bar = torch.mean(y)
+        return 1 - torch.sum((y - y_hat)**2)/torch.sum((y - y_bar)**2)
+    
+# define gradient descent optimizer class
+class GradientDescentOptimizer:
+
+    def __init__(self, model):
+        self.model = model 
+    
+    def step(self, X, y, alpha):
+        """
+        Compute one iteration of gradient descent using the feature matrix X 
+        and target vector y. 
+
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix.
+
+            y, torch.Tensor: the target vector.
+
+            alpha, float: the learning rate for gradient descent.
+        """
+
+        # Initialize a if needed
+        if self.model.w is None: 
+            self.model.w = torch.rand((X.size()[1]), dtype = torch.float64)
+
+        # Update w
+        self.model.w -= alpha*self.model.grad(X, y)
+        
+
 
 # Define ridge regression class
 class RidgeRegression:
@@ -18,16 +86,9 @@ class RidgeRegression:
     def pred(self, X):
         return (X*self.w).sum(axis = 1)
     
-    def score(self, X, y):
+    def r2(self, X, y):
         # https://www.ncl.ac.uk/webtemplate/ask-assets/external/maths-resources/statistics/regression-and-correlation/coefficient-of-determination-r-squared.html
         # R^2 value
         y_hat = self.pred(X)
         y_bar = torch.mean(y)
         return 1 - torch.sum((y - y_hat)**2)/torch.sum((y - y_bar)**2)
-    
-    # coefs standard error
-    # coefs test statistic
-    # coefs p-val
-
-    # adjusted R^2?
-
