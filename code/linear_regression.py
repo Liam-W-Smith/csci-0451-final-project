26124ae09e9fc5bef6779a048972ac68af38b37d
Liam-W-Smith <90705654+Liam-W-Smith@users.noreply.github.com>
2024-05-13 16:51:02 -0400

    linear regression

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -127,29 +127,3 @@ class GradientDescentOptimizer:
 
         # Update w
         self.model.w -= alpha*self.model.grad(X, y)
-        
-
-
-# Define ridge regression class
-class RidgeRegression:
-
-    def __init__(self):
-        self.w = None 
-
-    def fit(self, X, y, lam = 1.0):
-        p = X.size(1)
-        w_hat = torch.inverse(X.T@X + lam*torch.eye(p))@X.T@y
-        self.w = w_hat
-
-    def mse(self, X, y):
-        return ((X@self.w - y)**2).mean()
-    
-    def pred(self, X):
-        return (X*self.w).sum(axis = 1)
-    
-    def r2(self, X, y):
-        # https://www.ncl.ac.uk/webtemplate/ask-assets/external/maths-resources/statistics/regression-and-correlation/coefficient-of-determination-r-squared.html
-        # R^2 value
-        y_hat = self.pred(X)
-        y_bar = torch.mean(y)
-        return 1 - torch.sum((y - y_hat)**2)/torch.sum((y - y_bar)**2)
